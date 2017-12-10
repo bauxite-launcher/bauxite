a2ef8de8ab4d2242ea7ae9f1edaefda8d19ae528
Jim O'Brien <jim@jimmed.net>
2017-12-10 00:36:07 +0000

    [minecraft-assets]: Add serve command to test GraphQL endpoint

---
+++ packages/minecraft-assets/bin/serve.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const express = require('express')
+const { graphqlExpress } = require('apollo-server-express')
+const bodyParser = require('body-parser')
+const { makeSchema } = require('../graphql/schema')
+
+const launchServer = async (port = 2501) => {
+  const app = express()
+  const schema = await makeSchema()
+  app.use('/', bodyParser.json(), graphqlExpress({ schema }))
+  await new Promise(resolve => app.listen(port, resolve))
+  return { ...app, port }
+}
+if (module.parent) {
+  module.exports = { launchServer }
+} else {
+  launchServer(process.env.PORT).then(({ port }) =>
+    console.log(`Server listening at http://localhost:${port}`)
+  )
+}
