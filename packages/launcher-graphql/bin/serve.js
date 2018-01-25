7d321ac0c8f37ba06e5f651961e2711ff901a7cb
Jim O'Brien <jim@jimmed.net>
2018-01-25 03:11:28 +0000

    [launcher-graphql] Add GraphiQL endpoint

--- packages/launcher-graphql/bin/serve.js
+++ packages/launcher-graphql/bin/serve.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 const express = require('express')
-const { graphqlExpress } = require('apollo-server-express')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const bodyParser = require('body-parser')
+const open = require('open')
 const { makeSchema } = require('../lib/schema')
 
 const launchServer = async (port = 2501) => {
   const app = express()
   const schema = await makeSchema()
-  app.use('/', bodyParser.json(), graphqlExpress({ schema }))
+  app.use('/api', bodyParser.json(), graphqlExpress({ schema }))
+  app.use('/', graphiqlExpress({ endpointURL: '/api' }))
   await new Promise(resolve => app.listen(port, resolve))
   return { ...app, port }
 }
@@ -16,7 +18,13 @@ if (module.parent) {
   module.exports = { launchServer }
 } else {
   launchServer(process.env.PORT).then(
-    ({ port }) => console.log(`Server listening at http://localhost:${port}`),
+    ({ port }) => {
+      const url = `http://0.0.0.0:${port}`
+      console.log(`GraphiQL UI at ${url}/`)
+      console.log(`GraphQL endpoint at: ${url}/api`)
+      console.log('Press Ctrl + C to close')
+      open(url)
+    },
     error =>
       console.error(
         `Could not launch server, because ${error.stack || error.valueOf()}`
